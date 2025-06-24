ddc1328051dd4a0e0113020dbda0a4aa449a5040
Rohit Rathod <122153964+rohitrath0d@users.noreply.github.com>
2025-06-24 07:41:45 +0530

    Update page.tsx

--- client/src/app/listing/[id]/page.tsx
+++ client/src/app/listing/[id]/page.tsx
@@ -8,20 +8,26 @@ import ProductDetailsContent from "../[id]/productDetails";
 //     id: string;
 //   };  
 // }
-type PageProps = {
-     params: {
-    id: string;
-  };  
-}
 
+// type PageProps = {
+//      params: {
+//     id: string;
+//   };  
+// }
+
+// ✅ Define the async type
+type Params = Promise<{ id: string }>;
 
 // function ProductDetailsPage({ params }: { params: { id: string } }) {
-// function ProductDetailsPage({ params }: { params: { id: string } }) {
-function ProductDetailsPage({ params }: PageProps) {
-
+// function ProductDetailsPage({ params }: PageProps) {
+async function ProductDetailsPage({ params }: { params: Params }) {
+       
+     const { id } = await params;
+     
   return (
     <Suspense fallback={<ProductDetailsSkeleton />}>
-      <ProductDetailsContent id={params.id} />
+{/*       <ProductDetailsContent id={params.id} /> */}
+      <ProductDetailsContent id={id} />
     </Suspense>
   );
 }
