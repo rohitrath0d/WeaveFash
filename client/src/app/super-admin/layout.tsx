6a66d75ba6f116a2d1ca5eaee3ee20f41795cc14
Rohit Rathod <122153964+rohitrath0d@users.noreply.github.com>
2025-06-24 09:02:51 +0530

    Update layout.tsx

--- client/src/app/super-admin/layout.tsx
+++ client/src/app/super-admin/layout.tsx
@@ -5,30 +5,34 @@ import { useState } from 'react'
 
 
 import React from 'react'
+import { Suspense } from 'react';
+import SuperAdminManageProductPage from "@/app/super-admin/products/add/page";
 
-function SuperAdminLayout({children}: {children: React.ReactNode}) {
-    
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-    const handleToggle = () => setIsSidebarOpen(!isSidebarOpen);
+function SuperAdminLayout({ children }: { children: React.ReactNode }) {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const handleToggle = () => setIsSidebarOpen(!isSidebarOpen);
 
   return (
     <div className='min-h-screen bg-background'>
-
-        <SuperAdminSideBar
+      <SuperAdminSideBar
         //error will be shown, when passing it without the SidebarProps properties. Hence isOpen and toggle have to be mentioned here.
         isOpen={isSidebarOpen}
         // 1st way: 
         // toggle={() => setIsSidebarOpen(!isSidebarOpen)}
         toggle={() => setIsSidebarOpen(!isSidebarOpen)}
-        />
-        
-        <div className= {cn('transition-all duration-300', isSidebarOpen ? 'ml-64' : 'ml-16', 'min-h-screen' )}>
-            {/*  in this, here, all the children nodes will be rendered. */}
-            {/*  children --> child pages in sidebar --> which will be a full-fledged rendered component */}
+      />
 
-            {children}
-        
-        </div>
+      <div className={cn('transition-all duration-300', isSidebarOpen ? 'ml-64' : 'ml-16', 'min-h-screen')}>
+        {/*  in this, here, all the children nodes will be rendered. */}
+        {/*  children --> child pages in sidebar --> which will be a full-fledged rendered component */}
+
+        {/* Wrap only the children in Suspense */}
+        <Suspense fallback={<div>Loading...</div>}>
+          {children}
+        </Suspense>
+
+      </div>
     </div>
   )
 }
