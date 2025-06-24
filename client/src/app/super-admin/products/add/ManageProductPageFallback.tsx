e29f89e771db696fed4e1a5ded9fd85f23eec735
Rohit Rathod <122153964+rohitrath0d@users.noreply.github.com>
2025-06-24 08:23:17 +0530

    Update ManageProductPageFallback.tsx

--- client/src/app/super-admin/products/add/ManageProductPageFallback.tsx
+++ client/src/app/super-admin/products/add/ManageProductPageFallback.tsx
@@ -1,14 +1,29 @@
-import SuspenseBoundary from "@/SuspenseBoundary";
+// import SuspenseBoundary from "@/SuspenseBoundary";
+// import SuperAdminManageProductPage  from "@/app/super-admin/products/add/page";
+
+// export default function ManageProductPageFallback() {
+//   return (
+//     <div className="p-4">
+//       <h1 className="text-2xl font-bold mb-4">Add Product</h1>
+
+//       <SuspenseBoundary fallback={<p>Loading form...</p>}>
+//         <SuperAdminManageProductPage />
+//       </SuspenseBoundary>
+//     </div>
+//   );
+// }
+
+import { Suspense } from 'react';
 import SuperAdminManageProductPage  from "@/app/super-admin/products/add/page";
 
-export default function ManageProductPageFallback() {
+
+export default function AddProductPage() {
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Add Product</h1>
-
-      <SuspenseBoundary fallback={<p>Loading form...</p>}>
+      <Suspense fallback={<p>Loading form...</p>}>
         <SuperAdminManageProductPage />
-      </SuspenseBoundary>
+      </Suspense>
     </div>
   );
 }
