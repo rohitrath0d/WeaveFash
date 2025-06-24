65dcf6b07944190b6e394aef537143c6bf16c954
Rohit Rathod <122153964+rohitrath0d@users.noreply.github.com>
2025-06-24 06:48:22 +0530

    Update api.ts

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://localhost:3001'       // bcoz the backend is running on this port 
+export const API_BASE_URL = 'https://weavefash.onrender.com'       // bcoz the backend is running on this port 
 
 export const API_ROUTES = {
     AUTH: `${API_BASE_URL}/api/auth`,        // why? bcoz when we go to, server/server.ts -> we will see the endpoint we're using is [app.use("/api/auth", authRoutes)]
