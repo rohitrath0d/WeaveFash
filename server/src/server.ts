5b3f8411eb1e757e3832dfd4ed2f629606c5ce64
Rohit Rathod <122153964+rohitrath0d@users.noreply.github.com>
2025-07-04 23:46:21 +0530

    Update server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 3001;
 // enable cors & corsOptions
 const corsOptions    = {
     // origin:'http://localhost:3000',
-    origin:'https://weave-fash.vercel.app',
+    origin:'process.env.CLIENT_BASE_URL',
     credentials: true,                                      // as we are using cookie, credentials will be true.
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
